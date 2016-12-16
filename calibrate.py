19af63b917dc4355ba6cd66a1bf26faf8553191b
serranoycandela <serranoycandela@gmail.com>
2016-12-16 14:10:35 -0600

    scenario file generation from template

--- calibrate.py
+++ calibrate.py
@@ -2,6 +2,7 @@
 
 import argparse
 import sys
+from jinja2 import Template
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--parameters', type=argparse.FileType('r'),
@@ -13,11 +14,40 @@ parser.add_argument('--input_dir',
 parser.add_argument('--output_dir',
                     required=True,
                     help='path to output dir')
+parser.add_argument('--urban', type=argparse.FileType('r'),
+                    required=True,
+                    nargs="+",
+                    help='one or more urban gifs')
+parser.add_argument('--roads', type=argparse.FileType('r'),
+                    required=True,
+                    nargs="+",
+                    help='one or more roads gifs')
+parser.add_argument('--exclude', type=argparse.FileType('r'),
+                    required=True,
+                    help='exclude gif')
+parser.add_argument('--slope', type=argparse.FileType('r'),
+                    required=True,
+                    help='slope gif')
+parser.add_argument('--hillshade', type=argparse.FileType('r'),
+                    required=True,
+                    help='hillshade gif')
+parser.add_argument('--predict_start',
+                    type=int,
+                    required=True,
+                    help='year of prediction start')
+parser.add_argument('--predict_end',
+                    type=int,
+                    required=True,
+                    help='year of prediction end')
+parser.add_argument('--template', type=argparse.FileType('r'),
+                    required=True,
+                    help='path to screnario template file')
+
 args = parser.parse_args()
 
 
 for line in args.parameters.readlines():
-    (product, run, diff, brd, sprd, slp, rg) = line.strip().split()
+    (diff, brd, sprd, slp, rg) = line.strip().split()
     diff = int(diff)
     brd = int(brd)
     sprd = int(sprd)
@@ -31,7 +61,7 @@ def rescale(par):
         return 11
     else:
         return par
-    
+
 diff_start = rescale(diff) - 10
 diff_end = rescale(diff) + 10
 
@@ -47,136 +77,29 @@ slp_end = rescale(slp )+ 10
 rg_start = rescale(rg) - 10
 rg_end = rescale(rg) + 10
 
-
-    
-print "(product, run, diff, brd, sprd, slp, rg) "
-print     (product, run, diff, brd, sprd, slp, rg) 
-print "diff_start, diff_end, brd_start, brd_end, sprd_start, sprd_end, slp_start, slp_end, rg_start, rg_end"
-print diff_start, diff_end, brd_start, brd_end, sprd_start, sprd_end, slp_start, slp_end, rg_start, rg_end
-
-
-
-"""
-INPUT_DIR={input_dir
-OUTPUT_DIR={output_dir
-WHIRLGIF_BINARY=/srv/home/fidel/sleuth/Whirlgif/whirlgif 
-ECHO(YES/NO)=yes 
-WRITE_COEFF_FILE(YES/NO)=yes
-WRITE_AVG_FILE(YES/NO)=yes
-WRITE_STD_DEV_FILE(YES/NO)=yes 
-WRITE_MEMORY_MAP(YES/NO)=yes
-LOGGING(YES/NO)=YES
-LOG_LANDCLASS_SUMMARY(YES/NO)=yes 
-LOG_SLOPE_WEIGHTS(YES/NO)=no 
-LOG_READS(YES/NO)=no
-LOG_WRITES(YES/NO)=no
-LOG_COLORTABLES(YES/NO)=no
-LOG_PROCESSING_STATUS(0:off/1:low verbosity/2:high verbosity)=1 
-LOG_TRANSITION_MATRIX(YES/NO)=no
-LOG_URBANIZATION_ATTEMPTS(YES/NO)=no 
-LOG_INITIAL_COEFFICIENTS(YES/NO)=no 
-LOG_BASE_STATISTICS(YES/NO)=yes 
-LOG_DEBUG(YES/NO)= no
-LOG_TIMINGS(0:off/1:low verbosity/2:high verbosity)=1
-NUM_WORKING_GRIDS=8
-RANDOM_SEED=1
-MONTE_CARLO_ITERATIONS=5
-
-CALIBRATION_DIFFUSION_START= 0 
-CALIBRATION_DIFFUSION_STEP=  25 
-CALIBRATION_DIFFUSION_STOP=  100 
-
-CALIBRATION_BREED_START=     0 
-CALIBRATION_BREED_STEP=      25 
-CALIBRATION_BREED_STOP=      100 
-
-CALIBRATION_SPREAD_START=    0 
-CALIBRATION_SPREAD_STEP=     25 
-CALIBRATION_SPREAD_STOP=     100 
-
-CALIBRATION_SLOPE_START=     0 
-CALIBRATION_SLOPE_STEP=      25 
-CALIBRATION_SLOPE_STOP=      100 
-
-CALIBRATION_ROAD_START=      0 
-CALIBRATION_ROAD_STEP=       25 
-CALIBRATION_ROAD_STOP=       100 
-
-PREDICTION_DIFFUSION_BEST_FIT=  20 
-PREDICTION_BREED_BEST_FIT=  20 
-PREDICTION_SPREAD_BEST_FIT=  20 
-PREDICTION_SLOPE_BEST_FIT=  20 
-PREDICTION_ROAD_BEST_FIT=  20 
-
-PREDICTION_START_DATE=2000 
-PREDICTION_STOP_DATE=2025 
-
-URBAN_DATA= cmex250.urban.1960.gif 
-URBAN_DATA= cmex250.urban.1982.gif 
-URBAN_DATA= cmex250.urban.1997.gif 
-URBAN_DATA= cmex250.urban.2000.gif 
-
-ROAD_DATA= cmex250.roads.1960.gif 
-ROAD_DATA= cmex250.roads.1982.gif 
-ROAD_DATA= cmex250.roads.1997.gif 
-ROAD_DATA= cmex250.roads.2000.gif 
-
-EXCLUDED_DATA= cmex250.excluded.gif 
-
-SLOPE_DATA= cmex250.slope.gif 
-
-BACKGROUND_DATA= cmex250.hillshade.gif 
-
-WRITE_COLOR_KEY_IMAGES(YES/NO)=no 
-ECHO_IMAGE_FILES(YES/NO)=no
-ANIMATION(YES/NO)= no 
-
-DATE_COLOR=     0XFFFFFF #white 
-
-SEED_COLOR=  249, 209, 110 #pale yellow 
-
-WATER_COLOR=  20, 52, 214 # royal blue
-
-PROBABILITY_COLOR=   0,    50,         , #transparent 
-PROBABILITY_COLOR=   50,   60, 0X005A00, #0, 90,0 dark green
-PROBABILITY_COLOR=   60,   70, 0X008200, #0,130,0 
-PROBABILITY_COLOR=   70,   80, 0X00AA00, #0,170,0 
-PROBABILITY_COLOR=   80,   90, 0X00D200, #0,210,0 
-PROBABILITY_COLOR=   90,   95, 0X00FF00, #0,255,0 light green
-PROBABILITY_COLOR=   95,  100, 0X8B0000, #dark red 
-
-LANDUSE_CLASS=  0,  Unclass , UNC   , 0X000000 
-LANDUSE_CLASS=  1,  Urban   , URB   , 0X8b2323 #dark red
-LANDUSE_CLASS=  2,  Agric   ,       , 0Xffec8b #pale yellow 
-LANDUSE_CLASS=  3,  Range   ,       , 0Xee9a49 #tan 
-LANDUSE_CLASS=  4,  Forest  ,       , 0X006400 
-LANDUSE_CLASS=  5,  Water   , EXC   , 0X104e8b 
-LANDUSE_CLASS=  6,  Wetland ,       , 0X483d8b 
-LANDUSE_CLASS=  7,  Barren  ,       , 0Xeec591 
-
-VIEW_GROWTH_TYPES(YES/NO)=NO 
-GROWTH_TYPE_PRINT_WINDOW=0,0,0,0,1995,2020 
-PHASE0G_GROWTH_COLOR=  0xff0000 # seed urban area 
-PHASE1G_GROWTH_COLOR=  0X00ff00 # diffusion growth 
-PHASE2G_GROWTH_COLOR=  0X0000ff # NOT USED 
-PHASE3G_GROWTH_COLOR=  0Xffff00 # breed growth 
-PHASE4G_GROWTH_COLOR=  0Xffffff # spread growth 
-PHASE5G_GROWTH_COLOR=  0X00ffff # road influenced growth 
-
-VIEW_DELTATRON_AGING(YES/NO)=NO 
-DELTATRON_PRINT_WINDOW=0,0,0,0,1930,2020 
-DELTATRON_COLOR=  0x000000 # index 0 No or dead deltatron 
-DELTATRON_COLOR=  0X00FF00 # index 1 age = 1 year 
-DELTATRON_COLOR=  0X00D200 # index 2 age = 2 year 
-DELTATRON_COLOR=  0X00AA00 # index 3 age = 3 year 
-DELTATRON_COLOR=  0X008200 # index 4 age = 4 year 
-DELTATRON_COLOR=  0X005A00 # index 5 age = 5 year 
-
-ROAD_GRAV_SENSITIVITY=0.01 
-SLOPE_SENSITIVITY=0.1 
-CRITICAL_LOW=0.97 
-CRITICAL_HIGH=1.3 
-
-CRITICAL_SLOPE=21.0 
-BOOM=1.01 
-BUST=0.9"""
+template = Template(args.template.read())
+print template.render(
+    input_dir=args.input_dir,
+    output_dir=args.output_dir,
+    diff_start=diff_start,
+    diff_end=diff_end,
+    brd_start=brd_start,
+    brd_end=brd_end,
+    sprd_start=sprd_start,
+    sprd_end=sprd_end,
+    slp_start=slp_start,
+    slp_end=slp_end,
+    rg_start=rg_start,
+    rg_end=rg_end,
+    diff=diff,
+    brd=brd,
+    sprd=sprd,
+    slp=slp,
+    rg=rg,
+    urban=[p.name for p in args.urban],
+    roads=[p.name for p in args.roads],    
+    predict_start=args.predict_start,
+    predict_end=args.predict_end,
+    exclude=args.exclude.name,
+    slope=args.slope.name,
+    hillshade=args.hillshade.name)
--- extract_params.sh
+++ extract_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-awk '{print $2,$0}' $1  | sort -r |  awk '{print $1,$2,$16,$17,$18,$19,$20}' | head -n +3 | tail -n 1
+awk '{print $2,$0}' $1  | sort -r |  awk '{print $16,$17,$18,$19,$20}' | head -n +3 | tail -n 1
