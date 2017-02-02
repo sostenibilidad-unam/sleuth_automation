c523e13614f87aa41905eb755765266a34aeffac
Fidel Serrano <serranoycandela@gmail.com>
2017-02-02 16:28:15 -0600

    test stub for controlStats class

---
+++ tests/test_controlstats.py
@@ -0,0 +1,74 @@
+from sleuth_automation.controlstats import ControlStats
+
+from pprint import pprint
+def test_prueba():
+    assert 1 == 1
+
+def test_params():
+    cs = ControlStats('control_stats_fine.log', 5)
+
+
+
+
+    #scenario.fine
+    # PREDICTION_DIFFUSION_BEST_FIT=  1
+    # CALIBRATION_DIFFUSION_START= 0
+    # CALIBRATION_DIFFUSION_STEP=  5
+    # CALIBRATION_DIFFUSION_STOP=  20 
+
+    # PREDICTION_BREED_BEST_FIT=  1
+    # CALIBRATION_BREED_START=     0
+    # CALIBRATION_BREED_STEP=      5
+    # CALIBRATION_BREED_STOP=      20
+    
+    # PREDICTION_SPREAD_BEST_FIT=  100
+    # CALIBRATION_SPREAD_START=    75
+    # CALIBRATION_SPREAD_STEP=     6
+    # CALIBRATION_SPREAD_STOP=     99 
+
+    # PREDICTION_SLOPE_BEST_FIT=  100
+    # CALIBRATION_SLOPE_START=     50
+    # CALIBRATION_SLOPE_STEP=      12
+    # CALIBRATION_SLOPE_STOP=      98 
+
+    # PREDICTION_ROAD_BEST_FIT=  25
+    # CALIBRATION_ROAD_START=      0
+    # CALIBRATION_ROAD_STEP=       25
+    # CALIBRATION_ROAD_STOP=       100
+
+
+
+
+
+    #scenario.final
+    # CALIBRATION_DIFFUSION_START= 0
+    # CALIBRATION_DIFFUSION_STEP=  1
+    # CALIBRATION_DIFFUSION_STOP=  4 
+
+    # CALIBRATION_BREED_START=     0
+    # CALIBRATION_BREED_STEP=      1
+    # CALIBRATION_BREED_STOP=      4 
+
+    # CALIBRATION_SPREAD_START=    87
+    # CALIBRATION_SPREAD_STEP=     3
+    # CALIBRATION_SPREAD_STOP=     99 
+    
+    # CALIBRATION_SLOPE_START=     62
+    # CALIBRATION_SLOPE_STEP=      6
+    # CALIBRATION_SLOPE_STOP=      86 
+    
+    # CALIBRATION_ROAD_START=      0
+    # CALIBRATION_ROAD_STEP=       25
+    # CALIBRATION_ROAD_STOP=       100
+
+    # PREDICTION_DIFFUSION_BEST_FIT=  1
+    # PREDICTION_BREED_BEST_FIT=  1
+    # PREDICTION_SPREAD_BEST_FIT=  93
+    # PREDICTION_SLOPE_BEST_FIT=  62
+    # PREDICTION_ROAD_BEST_FIT=  50
+
+
+ 
+    
+    
+    
