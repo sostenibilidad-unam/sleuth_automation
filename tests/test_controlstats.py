b9900ef1d3fa2941b2f4b5a815fb1c69bded0dd0
Fidel Serrano <serranoycandela@gmail.com>
2017-02-07 12:35:30 -0600

    test setting of start, end and step for all params

--- tests/test_controlstats.py
+++ tests/test_controlstats.py
@@ -1,74 +1,122 @@
 from sleuth_automation.controlstats import ControlStats
 
-from pprint import pprint
-def test_prueba():
-    assert 1 == 1
+def test_params_right_start_and_step_trivial_1s():
+    cs = ControlStats('tests/control_stats.log_1s', 1)
 
-def test_params():
-    cs = ControlStats('control_stats_fine.log', 5)
+    assert cs.params == {'brd': 1,
+                         'brd_end': 4,
+                         'brd_start': 0,
+                         'brd_step': 1,
+                         'diff': 1,
+                         'diff_end': 4,
+                         'diff_start': 0,
+                         'diff_step': 1,
+                         'rg': 1,
+                         'rg_end': 4,
+                         'rg_start': 0,
+                         'rg_step': 1,
+                         'slp': 1,
+                         'slp_end': 4,
+                         'slp_start': 0,
+                         'slp_step': 1,
+                         'sprd': 1,
+                         'sprd_end': 4,
+                         'sprd_start': 0,
+                         'sprd_step': 1}
 
+    cs = ControlStats('tests/control_stats.log_1s', 5)
 
-
-
-    #scenario.fine
-    # PREDICTION_DIFFUSION_BEST_FIT=  1
-    # CALIBRATION_DIFFUSION_START= 0
-    # CALIBRATION_DIFFUSION_STEP=  5
-    # CALIBRATION_DIFFUSION_STOP=  20 
-
-    # PREDICTION_BREED_BEST_FIT=  1
-    # CALIBRATION_BREED_START=     0
-    # CALIBRATION_BREED_STEP=      5
-    # CALIBRATION_BREED_STOP=      20
-    
-    # PREDICTION_SPREAD_BEST_FIT=  100
-    # CALIBRATION_SPREAD_START=    75
-    # CALIBRATION_SPREAD_STEP=     6
-    # CALIBRATION_SPREAD_STOP=     99 
-
-    # PREDICTION_SLOPE_BEST_FIT=  100
-    # CALIBRATION_SLOPE_START=     50
-    # CALIBRATION_SLOPE_STEP=      12
-    # CALIBRATION_SLOPE_STOP=      98 
-
-    # PREDICTION_ROAD_BEST_FIT=  25
-    # CALIBRATION_ROAD_START=      0
-    # CALIBRATION_ROAD_STEP=       25
-    # CALIBRATION_ROAD_STOP=       100
-
-
-
-
-
-    #scenario.final
-    # CALIBRATION_DIFFUSION_START= 0
-    # CALIBRATION_DIFFUSION_STEP=  1
-    # CALIBRATION_DIFFUSION_STOP=  4 
-
-    # CALIBRATION_BREED_START=     0
-    # CALIBRATION_BREED_STEP=      1
-    # CALIBRATION_BREED_STOP=      4 
-
-    # CALIBRATION_SPREAD_START=    87
-    # CALIBRATION_SPREAD_STEP=     3
-    # CALIBRATION_SPREAD_STOP=     99 
-    
-    # CALIBRATION_SLOPE_START=     62
-    # CALIBRATION_SLOPE_STEP=      6
-    # CALIBRATION_SLOPE_STOP=      86 
-    
-    # CALIBRATION_ROAD_START=      0
-    # CALIBRATION_ROAD_STEP=       25
-    # CALIBRATION_ROAD_STOP=       100
-
-    # PREDICTION_DIFFUSION_BEST_FIT=  1
-    # PREDICTION_BREED_BEST_FIT=  1
-    # PREDICTION_SPREAD_BEST_FIT=  93
-    # PREDICTION_SLOPE_BEST_FIT=  62
-    # PREDICTION_ROAD_BEST_FIT=  50
+    assert cs.params ==  {'brd': 1,
+                          'brd_end': 20,
+                          'brd_start': 0,
+                          'brd_step': 5,
+                          'diff': 1,
+                          'diff_end': 20,
+                          'diff_start': 0,
+                          'diff_step': 5,
+                          'rg': 1,
+                          'rg_end': 20,
+                          'rg_start': 0,
+                          'rg_step': 5,
+                          'slp': 1,
+                          'slp_end': 20,
+                          'slp_start': 0,
+                          'slp_step': 5,
+                          'sprd': 1,
+                          'sprd_end': 20,
+                          'sprd_start': 0,
+                          'sprd_step': 5}
 
 
- 
-    
-    
-    
+
+def test_params_right_start_and_step_trivial_100s():
+    cs = ControlStats('tests/control_stats.log_100s', 5)
+
+    assert cs.params == {'brd': 100,
+                         'brd_end': 100,
+                         'brd_start': 80,
+                         'brd_step': 5,
+                         'diff': 100,
+                         'diff_end': 100,
+                         'diff_start': 80,
+                         'diff_step': 5,
+                         'rg': 100,
+                         'rg_end': 100,
+                         'rg_start': 80,
+                         'rg_step': 5,
+                         'slp': 100,
+                         'slp_end': 100,
+                         'slp_start': 80,
+                         'slp_step': 5,
+                         'sprd': 100,
+                         'sprd_end': 100,
+                         'sprd_start': 80,
+                         'sprd_step': 5}
+    cs = ControlStats('tests/control_stats.log_100s', 1)
+
+    assert cs.params == {'brd': 100,
+                         'brd_end': 100,
+                         'brd_start': 96,
+                         'brd_step': 1,
+                         'diff': 100,
+                         'diff_end': 100,
+                         'diff_start': 96,
+                         'diff_step': 1,
+                         'rg': 100,
+                         'rg_end': 100,
+                         'rg_start': 96,
+                         'rg_step': 1,
+                         'slp': 100,
+                         'slp_end': 100,
+                         'slp_start': 96,
+                         'slp_step': 1,
+                         'sprd': 100,
+                         'sprd_end': 100,
+                         'sprd_start': 96,
+                         'sprd_step': 1}
+
+
+
+def test_params_right_start_and_step_25_100():
+    cs = ControlStats('tests/control_stats.log_25_100', 5)
+
+    assert cs.params == {'brd': 25,
+                         'brd_end': 97,
+                         'brd_start': 25,
+                         'brd_step': 18,
+                         'diff': 25,
+                         'diff_end': 97,
+                         'diff_start': 25,
+                         'diff_step': 18,
+                         'rg': 25,
+                         'rg_end': 97,
+                         'rg_start': 25,
+                         'rg_step': 18,
+                         'slp': 25,
+                         'slp_end': 97,
+                         'slp_start': 25,
+                         'slp_step': 18,
+                         'sprd': 25,
+                         'sprd_end': 97,
+                         'sprd_start': 25,
+                         'sprd_step': 18}
