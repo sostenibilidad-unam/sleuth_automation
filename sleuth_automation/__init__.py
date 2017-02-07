ca32b8cf761a7393b29606998ebcbc25c38cf4e2
Fidel Serrano <serranoycandela@gmail.com>
2017-02-07 13:10:03 -0600

    interpolate params in scenario template

--- sleuth_automation/__init__.py
+++ sleuth_automation/__init__.py
@@ -4,7 +4,7 @@ from controlstats import ControlStats
 
 class Location:
 
-    def __init__(self, location, input_path, output_path, start, end, dates):
+    def __init__(self, location, input_path, output_path, predict_start, predict_end, dates):
         """
         location is a name for the location according to sleuth docs
         path is a path to the location directory
@@ -13,9 +13,10 @@ class Location:
         
         """
         self.location = location
-        self.path = path
-        self.start = start
-        self.end = end
+        self.input_path = input_path
+        self.output_path = output_path        
+        self.predict_start = start
+        self.predict_end = end
         self.dates = dates
 
         self.validate_path()
@@ -23,65 +24,67 @@ class Location:
     def validate_path(self):
         pass
    
-    def create_scenario_file(self, stage, monte_carlo_iterations):
-        # if stage == 'coarse':
-        #     pass
-        # elif stage == 'fine':
-        #     cs = ControlStats(os.path.join(os.path.join(self.output_path, stage), 'control_stats.log')
+    def create_scenario_file(self, params, monte_carlo_iterations):
         
-        # env = Environment(loader=PackageLoader('sleuth_automation', 'templates'))
-        # template = env.get_template('scenario.jinja')
+        env = Environment(loader=PackageLoader('sleuth_automation', 'templates'))
+        template = env.get_template('scenario.jinja')
 
-        # arguments = {input_dir: self.input_dir + "/",
-        #             output_dir: self.output_dir + "/"
-        #     monte_carlo_iterations=monte_carlo_iterations,
-        #     predict_start=args.predict_start,
-        #     predict_end=args.predict_end,
-        #     urban=[p.name.replace(args.input_dir+"/", '') for p in args.urban],
-        #     roads=[p.name.replace(args.input_dir+"/", '') for p in args.roads],
-        #     exclude=args.exclude.name.replace(args.input_dir+"/", ''),
-        #     slope=args.slope.name.replace(args.input_dir+"/", ''),
-        #     hillshade=args.hillshade.name.replace(args.input_dir+"/", '')
-        #             }
+        arguments = { 'input_dir': self.input_dir + "/",
+                      'output_dir': self.output_dir + "/",
+                      'monte_carlo_iterations': monte_carlo_iterations,
+                      'predict_start': self.predict_start,
+                      'predict_end': self.predict_end,
+                      'urban' : ["%s.urban.%s.gif" % (self.location, date) for date in self.dates],
+                      'roads': ["%s.roads.%s.gif" % (self.location, date) for date in self.dates],
+                      'exclude': os.path.join(self.input_path, "%s.excluded.gif" % self.location),
+                      'slope': os.path.join(self.input_path, "%s.slope.gif" % self.location),
+                      'hillshade': os.path.join(self.input_path, "%s.hillshade.gif" % self.location),
+                      }
 
-        # arguments.update(cs.params)
-        # template.render(arguments)        
-        pass
+        arguments.update(params)
+        template.render(arguments)        
 
-    def sleuth_calibrate_coarse(self):
-        diff = 50
-        diff_start = 0
-        diff_step = 25
-        diff_end = 100
 
-        brd = 50
-        brd_start = 0
-        brd_step = 25
-        brd_end = 100
+    def calibrate_coarse(self):
+        coarse_params = {'diff': 50,
+                         'diff_start': 0,
+                         'diff_step': 25,
+                         'diff_end': 100,
+                         'brd': 50,
+                         'brd_start': 0,
+                         'brd_step': 25,
+                         'brd_end': 100,
 
-        sprd = 50
-        sprd_start = 0
-        sprd_step = 25
-        sprd_end = 100
+                         'sprd': 50,
+                         'sprd_start': 0,
+                         'sprd_step': 25,
+                         'sprd_end': 100,
 
-        slp = 50
-        slp_start = 0
-        slp_step = 25
-        slp_end = 100
+                         'slp': 50,
+                         'slp_start': 0,
+                         'slp_step': 25,
+                         'slp_end': 100,
 
-        rg = 50
-        rg_start = 0
-        rg_step = 25
-        rg_end = 100
+                         'rg': 50,
+                         'rg_start': 0,
+                         'rg_step': 25,
+                         'rg_end': 100 }
+        self.create_scenario_file(coarse_params, 50)
 
+    def calibrate_fine(self):
+        cs = ControlStats(os.path.join(os.path.join(self.output_path, 'coarse'), 'control_stats.log'))
+        self.create_scenario_file(cs.params, 5)
+        
+    def calibrate_final(self):
+        cs = ControlStats(os.path.join(os.path.join(self.output_path, 'fine'), 'control_stats.log'))
+        self.create_scenario_file(cs.params, 1)
 
     def sleuth_calibrate(self, scenario_file_path, monte_carlo_iterations=5):
-        # aqui ajustar el step segun el stage
+        self.calibrate_coarse()
+        self.calibrate_fine()
+        self.calibrate_final()
 
-        self.create_scenario_file('coarse', monte_carlo_iterations)
-        self.create_scenario_file('fine', monte_carlo_iterations)
-        self.create_scenario_file('final', monte_carlo_iterations)
-        
+                
     def sleuth_predict(self, scenario_file_path, monte_carlo_iterations=150):
         passself.create_scenario_file('predict', monte_carlo_iterations)
     
