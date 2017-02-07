582d639525c6c71ebdeba3de54c0a818dc6c7499
Fidel Serrano <serranoycandela@gmail.com>
2017-02-07 12:36:14 -0600

    setuptools stub

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sleuth_automation',
+      version='0.1',
+      description='Python wrapper for comfy running of SLEUTH',
+      url='http://github.com/sostenibilidad-unam/sleuth_automation',
+      author='Fidel Serrano, Rodrigo Garcia',
+      author_email='rgarcia@iecologia.unam.mx',
+      license='GPLv3',
+      packages=['sleuth_automation'],
+      zip_safe=False)
