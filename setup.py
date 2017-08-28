aece59d9661a3153e980f389c484a80f03cecc7f
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2017-08-28 12:44:52 -0500

    further descriptions in setup

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='sleuth_automation',
-      version='0.2',
-      description='Python wrapper for comfy running of SLEUTH',
+      version='0.3',
+      description='Python wrapper for SLEUTH urban growth model.',
+      long_description=readme(),
+      classifiers=[
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering'],
       url='http://github.com/sostenibilidad-unam/sleuth_automation',
       author='Fidel Serrano, Rodrigo Garcia',
       author_email='rgarcia@iecologia.unam.mx',
       license='GPLv3',
       packages=['sleuth_automation'],
+      install_requires=['pandas', 'jinja2', 'sh'],
       scripts=['bin/sleuth_run.py',
                'bin/create_sleuth_condor_batch.py'],
       zip_safe=False)
